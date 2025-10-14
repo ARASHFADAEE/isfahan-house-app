6d4f7f6656ebba05fa34454349840b9d592758e4
ArashFadaee <92361081+ARASHFADAEE@users.noreply.github.com>
2025-10-14 15:20:25 -0400

    set main data dashboard and set livewire componnet dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->group(function(){
+
+
+    Route::get('/dashboard',Dashboard::class);
 });
